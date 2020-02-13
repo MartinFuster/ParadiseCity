9fbf8392bb8ac5beea3eb6286430748ee3dfb9b5
MartinFuster <martinfuster3@gmail.com>
2020-02-13 19:16:50 -0300

    initial flights search front

--- src/components/HeaderFlights.js
+++ src/components/HeaderFlights.js
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../img/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { addDays } from "date-fns";
 
 function Header() {
+  const [startDate, setStartDate] = useState(null);
   return (
     <header className="header">
       <Link to="/">
         <img src={logo} alt="logo" className="logo" />
       </Link>
       <form action="#" className="search">
-        <input
-          type="text"
-          className="search__input"
-          placeholder="Search Flights"
-        />
-        <button className="search__button">
-          <FontAwesomeIcon icon="search" className="search__icon-lupa" />
-        </button>
+        <div className="search-container">
+          <span className="search-indicator">Origin</span>
+          <input
+            type="text"
+            className="search-input-text"
+            placeholder="Enter your origin"
+          />
+        </div>
+        <div className="search-container">
+          <span className="search-indicator">Destintation</span>
+          <input
+            type="text"
+            className="search-input-text"
+            placeholder="Enter your destination"
+          />
+        </div>
+        <div className="search-container">
+          <span className="search-indicator">Departure</span>
+          <DatePicker
+            selected={startDate}
+            onChange={date => setStartDate(date)}
+            minDate={new Date()}
+            maxDate={addDays(new Date(), 365)}
+            placeholderText="Departure"
+          />
+        </div>
+        <div className="search-container">
+          <span className="search-indicator">Arrival</span>
+          <DatePicker
+            selected={startDate}
+            onChange={date => setStartDate(date)}
+            minDate={new Date()}
+            maxDate={addDays(new Date(), 365)}
+            placeholderText="Arrival"
+          />
+        </div>
+        <button>Search</button>
       </form>
       <nav className="user-nav">
         <div className="user-nav__icon-box">
